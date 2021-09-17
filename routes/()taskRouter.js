9b080662d44d67d02266da6c1553397724142f9c
Jtomat <stragnik13@gmail.com>
2021-09-17 19:37:11 +0500

    Update ()taskRouter.js

--- routes/()taskRouter.js
+++ routes/()taskRouter.js
@@ -4,7 +4,7 @@ const taskController = require('../controllers/()taskController');
 
 router.get('/user', taskController.getAll)
 router.get('/', taskController.getAll)
-router.get('/add', taskController.create)
+router.post('/add', taskController.create)
 router.get('/open/:id', taskController.getOne)
 router.delete('/remove/:id', taskController.delete)
 router.put('/edit/:id', taskController.update)
