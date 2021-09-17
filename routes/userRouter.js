240c958f7ca3a7f5bb5298d44a614c3c25362616
Jtomat <stragnik13@gmail.com>
2021-09-17 18:34:49 +0500

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const roleController = require('../controllers/()workerRoleController')
 const authMiddleware = require("../middleware/authMiddleware")
 
 router.post('/registration', userController.registration)
@@ -10,5 +11,9 @@ router.get('/',userController.getAll)
 router.get('/:id',userController.getOne)
 router.put('/', authMiddleware, userController.update)
 router.delete('/:id', authMiddleware, userController.delete)
+router.put('/conf/roles/:id', roleController.update)
+router.post('/conf/roles/', roleController.create)
+router.delete('/conf/roles/:id', roleController.delete)
+router.put('/conf/roles/:id', roleController.update)
 
 module.exports = router
