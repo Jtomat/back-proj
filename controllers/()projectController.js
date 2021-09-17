9702c41c47b5b1cbb43fc08241353f4a642a35fc
Jtomat <stragnik13@gmail.com>
2021-09-17 23:20:34 +0500

    Update ()projectController.js

--- controllers/()projectController.js
+++ controllers/()projectController.js
@@ -67,7 +67,6 @@ class ProjectController {
             return res.json(rest);
         }catch (e) {
             console.log(e)
-            return res.json({e})
         }
     }
     async _getAll(){
