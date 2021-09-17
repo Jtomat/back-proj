ee2ef6b4b95a2afc218e2bee87517c87c5c73a30
Jtomat <stragnik13@gmail.com>
2021-09-17 23:00:48 +0500

    Update ()workerRoleController.js

--- controllers/()workerRoleController.js
+++ controllers/()workerRoleController.js
@@ -44,7 +44,7 @@ class WorkerRoleController{
     }
     async _get(id){
         return  (await WorkerRole.findAll()).filter(item=>item.id===id)[0];
-        
+
     }
     async delete(req, res) {
         try {
